12dd7c22cdc6678af2fd438f665df76142ddcd00
Yuming Wang <yumwang@ebay.com>
2022-03-25 15:03:41 +0800

    Fix style

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/parser/PlanParserSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/parser/PlanParserSuite.scala
@@ -395,7 +395,7 @@ class PlanParserSuite extends AnalysisTest {
     // Grouping Sets
     assertEqual(s"$sql grouping sets((a, b), (a), ())",
       Aggregate(Seq(GroupingSets(Seq(Seq('a, 'b), Seq('a), Seq()), Seq('a, 'b))),
-        Seq('a, 'b, 'sum.function('c).as("c")), false,  table("d")))
+        Seq('a, 'b, 'sum.function('c).as("c")), false, table("d")))
 
     assertEqual(s"$sqlWithoutGroupBy group by grouping sets((a, b), (a), ())",
       Aggregate(Seq(GroupingSets(Seq(Seq('a, 'b), Seq('a), Seq()))),
