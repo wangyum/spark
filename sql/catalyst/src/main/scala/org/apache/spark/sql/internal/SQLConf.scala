2f9be5a5471f683c11a01d68fbdb53d3fac0099b
Yuming Wang <yumwang@ebay.com>
2022-05-13 13:50:03 +0800

    Revert `SPARK_GENERATE_GOLDEN_FILES=1 build/sbt "sql/testOnly *PlanStability*Suite"` result

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1532,7 +1532,7 @@ object SQLConf {
       " method.")
     .version("2.0.0")
     .booleanConf
-    .createWithDefault(false)
+    .createWithDefault(true)
 
   val WHOLESTAGE_CODEGEN_USE_ID_IN_CLASS_NAME =
     buildConf("spark.sql.codegen.useIdInClassName")
@@ -1561,7 +1561,7 @@ object SQLConf {
     .internal()
     .stringConf
     .checkValues(CodegenObjectFactoryMode.values.map(_.toString))
-    .createWithDefault(CodegenObjectFactoryMode.NO_CODEGEN.toString)
+    .createWithDefault(CodegenObjectFactoryMode.FALLBACK.toString)
 
   val CODEGEN_FALLBACK = buildConf("spark.sql.codegen.fallback")
     .internal()
